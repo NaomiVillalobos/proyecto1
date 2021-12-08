a4f0916c5cc399d99ede9c542abf7e54568dda54
hasseljp08 <85628082+hasseljp08@users.noreply.github.com>
2021-12-07 21:57:42 -0600

    Update Usuarios.vue

--- src/components/Usuarios.vue
+++ src/components/Usuarios.vue
@@ -1,1 +1,41 @@
+<script>
+export default {
+  data: () => ({
+    group: null,
+  }),
+  props : {
+      drawer : {
+        type : Boolean
+      }
+  },
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
+<template>
+  <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+    
+    <v-list nav dense>
+      <v-list-item-group
+        v-model="group"
+        active-class="deep-purple--text text--accent-4"
+      >
+        <v-list-item>
+          <v-list-item-title>
+            <router-link to="/Crear_Usuarios">Crear_Usuarios</router-link>
+          </v-list-item-title>
+        </v-list-item>
 
+        <v-list-item>
+          <v-list-item-title>
+            <router-link to="/Asignar_Roles">Asignar_Roles</router-link>
+          </v-list-item-title>
+        </v-list-item>
+
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
