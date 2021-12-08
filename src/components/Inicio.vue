71a5c68b95c8b83f29ea1123ce3fde3ffb04b5f0
hasseljp08 <85628082+hasseljp08@users.noreply.github.com>
2021-12-08 09:06:28 -0600

    Update Inicio.vue

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,3 +1,56 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+ 
+<title>Inicio</title>
+  <script src="https://unpkg.com/vue/dist/vue.js"></script>
+</head>
+  <body>
+ 
+ <div id="app">
+   <input v-model='search' />
+    <ul v-for="item in searchData ">
+    <li> {(item.name}}, usuario: {{item.usuario}} </ li>
+   </ul>
+ </div>
+ 
+ 
+<script>
+  var vm = new Vue({
+      el: '#app',
+      data: {
+    search: '',
+    products: [{
+         nombre: 'Hassell',
+    usuario : hasseljp08,"
+  }, 
+}]
+},
+computed: {
+searchData: function() {
+var search = this.search;
+ 
+if (search) {
+return this.products.filter(function(product) {
+return Object.keys(product).some(function(key) {
+return String(product[key]).toLowerCase().indexOf(search) > -1
+})
+})
+}
+ 
+return this.products;
+}
+}
+})
+</script>
+ 
+ 
+</body>
+</html>
+
+
 <div id="app">
 <h1>{{ message }} </h1>
 </div>
@@ -48,3 +101,46 @@ var example1 = new Vue({
     }
   })
 </script>
+
+
+<script>
+export default {
+  data: () => ({
+    group: null,
+  }),
+  props : {
+      drawer : {
+        type : Boolean
+      }
+  },
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
+<template>
+  <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+    
+    <v-list nav dense>
+      <v-list-item-group
+        v-model="group"
+        active-class="deep-purple--text text--accent-4"
+      >
+        <v-list-item>
+          <v-list-item-title>
+            <router-link to="/Crear_Cuenta">Crear_Cuenta</router-link>
+          </v-list-item-title>
+        </v-list-item>
+        
+         <v-list-item>
+          <v-list-item-title>
+            <router-link to="/E-Descargas_BackEnd">E-Descargas_BackEnd"</router-link>
+          </v-list-item-title>
+        </v-list-item>
+        
+  </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
