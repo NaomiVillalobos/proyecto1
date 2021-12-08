8631b51f970d4c11748930223ab8388506b27d5a
hasseljp08 <85628082+hasseljp08@users.noreply.github.com>
2021-12-07 20:35:10 -0600

    Update Inicio.vue

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -13,3 +13,38 @@ message: "Bienvenido a nuestra pagina"
 }
 })
 </script>
+
+
+<div id="aceptar">
+  <button v-on:click="counter += 1">Aceptar</button>
+  <p>Se ha hecho clic en el botón de arriba {{ counter }} veces.</p>
+</div>
+
+<div v-if= "ShowAlert">
+  <p> Veamos el menu principal </p>
+</div>
+
+var example1 = new Vue({
+  el: '#aceptar',
+  data: {
+    counter: 0
+  }
+})
+
+
+<div id= "app1">
+  <ul>
+    <li v-for="menuprincipal">
+      {{ menup }}
+    </li>
+  </ul>
+</div>
+
+<script>
+  new Vue({
+    el: '#app1',
+    data: {
+      menuprincipal: ["Seguridad", "Administracion", "Consultas"]
+    }
+  })
+</script>
