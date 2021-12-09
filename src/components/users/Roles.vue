f0b8b042172e1bbd5d5632d50ce41042bfd9f5c4
hasseljp08 <85628082+hasseljp08@users.noreply.github.com>
2021-12-09 16:15:37 -0600

    Update Roles.vue

--- src/components/users/Roles.vue
+++ src/components/users/Roles.vue
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8"></meta>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Hola mundo con Vue.js!</title>
+	<title>Roles</title>
 	<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 </head>
 <body>
