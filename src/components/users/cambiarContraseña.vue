a93b065aba6d936e08a89633a0848ebc1cbc6925
hasseljp08 <85628082+hasseljp08@users.noreply.github.com>
2021-12-09 16:46:14 -0600

    Update cambiarContraseña.vue

--- src/components/users/cambiarContraseña.vue
+++ src/components/users/cambiarContraseña.vue
@@ -1,1 +1,20 @@
+<template>
+  <div class= "mt-5">
+    
+    <h1>Cambiar Contraseña </h1>
+    
+    <form>
+    
+      <b-form-input type="text">Contraseña</b-form-input>
+      <b-form-input type="text">Nueva Contraseña</b-form-input>
+      <b-form-input type="text">Comfirmar Contraseña</b-form-input>
+      
+      <b-button variant="success">Cambiar Contraseña</b-button>
+      <b-button variant="danger">Cancelar</b-button>
+      
+    </form>
+  </div>
+</template>
 
+   
+      
