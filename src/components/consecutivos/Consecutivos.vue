26c8eee61b8874ae1cdc811500feb8dabcce778a
hasseljp08 <85628082+hasseljp08@users.noreply.github.com>
2021-12-09 21:57:09 -0600

    Update Consecutivos.vue

--- src/components/consecutivos/Consecutivos.vue
+++ src/components/consecutivos/Consecutivos.vue
@@ -1,1 +1,45 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Consecutivo</title>
+	<script type="text/javascript" src="vue.min.js"></script>
+</head>
+<body>
+<h1>Lista de Consecutivos</h1>
+<div id="app" class="container">
+<table border="1">
+<thead>
+	<th>Codigo</th>
+	<th>Descripcion</th>
+	<th>Consecutivo</th>
+	<th>Edicion</th>
+</thead>
+	<tr v-for="data in all_data ">
+		<td>{{data.codigo}}</td>
+		<td>{{data.descripcion}}</td>
+		<td>{{data.consecutivo}}</td>
+		<td>{{data.edicion}}</td>
+	</tr>
+</ul>
+</div>
+<script>
+	var app = new Vue({
+		el: "#app",
+		data: {
+			all_data:[]
+		},
+		created: function(){
+			console.log("Iniciando ...");
+			this.get_contacts();
+		},
+		methods:{
+			get_contacts: function(){
+				fetch("./lista.vue")
+				.then(response=>response.json())
+				.then(json=>{this.all_data=json.lista})
+			}
+		}
+	});
+</script>
+</body>
+</html>
