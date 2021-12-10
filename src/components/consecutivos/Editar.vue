af17e4f318fee2160b0bf5da74289f662cb9663d
hasseljp08 <85628082+hasseljp08@users.noreply.github.com>
2021-12-09 22:19:45 -0600

    Create Editar.vue

---
+++ src/components/consecutivos/Editar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mt-5">
+      <h1>Informacion del Consecutivo</h1>
+      
+      <form>
+      <p>Descripcion</p>
+      
+        <select v-model="selected">
+          <option v-for="option in options" v-bind:value="option.value">
+          {{ option.text }}
+          </option>
+        </select>
+        <span>Seleccionado: {{ selected }}</span>
+        new Vue({
+          el: '...',
+          data: {
+            selected: 'A',
+            options: [
+              { text: 'Peliculas', value: 'Peliculas },
+              { text: 'Libros', value: 'Libros' },
+              { text: 'Musica', value: 'Musica' }
+            ]
+          }
+        })
+        <b-form-input type="number">Consecutivo<b-form-input>
+        <p>Posee prefijo?</p>
+        <div id='prefijo'>
+        <input type="checkbox" id="si" value="Si" v-model="prefijocheck">
+        <label for="si">Si</label>
+       
+          <br>
+          <span>Respuesta: {{ prefijocheck }}</span>
+        </div>
+        new Vue({
+          el: '#prefijo',
+          data: {
+            prefijocheck: []
+          }
+        })
